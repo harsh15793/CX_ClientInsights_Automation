029d5b244f3d7c3bf160d86cb784b47be39ad338
harsh <harsh15793@gmail.com>
2022-12-22 00:40:18 +0530

    Commit on21 dec 2022

--- sentiment_analysis_transformer.py
+++ sentiment_analysis_transformer.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import xlsxwriter
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 from transformers import AutoTokenizer
@@ -10,13 +11,16 @@ from transformers import AutoModelForSequenceClassification
 from scipy.special import softmax
 from tqdm.notebook import tqdm
 
-
 MODEL = f'cardiffnlp/twitter-roberta-base-sentiment'
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
 plt.style.use('ggplot')
-df = pd.read_excel('files/Danske Bank .xlsx')
+#df = pd.read_excel('files/Danske Bank .xlsx')
+#workbook = xlsxwriter.Workbook('files/Danske Bank_sentiments.xlsx')
+df = pd.read_excel('files/U.S. Polo Assn. .xlsx')
+workbook = xlsxwriter.Workbook('files/U.S. Polo Assn_sentiments.xlsx')
+workbook.close()
 
 def polarity_ratings_roberta(example):
     encoded_text = tokenizer(example, return_tensors ='pt')
@@ -42,7 +46,10 @@ for i, row in tqdm(df.iterrows(), total=len(df)):
 results_df = pd.DataFrame(res).T
 results_df = results_df.reset_index().rename(columns = {'index' : 'Id'}).merge(df, how ='left')
 
-sns.pairplot(data = results_df,
+with pd.ExcelWriter('files/U.S. Polo Assn_sentiments.xlsx',mode="a",engine="openpyxl",if_sheet_exists="overlay") as writer:
+    results_df.to_excel(writer, sheet_name="Sheet1",index=False)
+
+fig = sns.pairplot(data = results_df,
             vars =['roberta_neg', 'roberta_neu', 'roberta_pos'],
             hue = 'Stars Given',
             palette = 'tab10')
@@ -50,3 +57,5 @@ sns.pairplot(data = results_df,
 
 #Example to check for wrongly assessed comments
 #print(results_df.query('`Stars Given` == 1').sort_values('roberta_pos', ascending = False)['Review Text'].values[0])
+#fig = plt.figure()
+#st.pyplot(fig)
